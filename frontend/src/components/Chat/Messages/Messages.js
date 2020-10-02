734204fa01b1cdfa3f7a92db7a10a1d4bc1482b0
kola <kvalic97@gmail.com>
2020-10-02 23:00:06 +0200

    implemented scroll down button for chat

--- frontend/src/components/Chat/Messages/Messages.js
+++ frontend/src/components/Chat/Messages/Messages.js
@@ -1,28 +1,66 @@
-import React, { useRef, useEffect } from "react"
+import React, { useRef, useState, useEffect } from "react"
 
 import Message from "./Message"
 
 import "./Messages.css"
 
 function Messages({ messages, user }) {
-	const msgRef = useRef()
+	const containerRef = useRef()
 	const lastScrollTop = useRef(0)
 
-	useEffect(() => {
-		// if user scrolled to top, don't scroll to bottom
-		if (lastScrollTop.current > msgRef.current.scrollTop) return
+	const [showScrollDown, setShowScrollDown] = useState(false)
+	const [dontImmediatelyScrollDown, setDontImmediatelyScrollDown] = useState(false)
 
-		lastScrollTop.current = msgRef.current.scrollTop
+	const scroll = () => {
+		lastScrollTop.current = containerRef.current.scrollTop
 
 		// scroll to bottom of overflowed div
-		msgRef.current.scrollTop = msgRef.current.scrollHeight
+		containerRef.current.scrollTop = containerRef.current.scrollHeight
+
+		setShowScrollDown(false)
+	}
+
+	const handleScroll = () => {
+		if (lastScrollTop.current > containerRef.current.scrollTop) {
+			setShowScrollDown(true)
+		} 
+		else if (lastScrollTop.current <= containerRef.current.scrollTop && showScrollDown) {
+			setDontImmediatelyScrollDown(true)
+			setShowScrollDown(false)
+		} 
+		else if (dontImmediatelyScrollDown) {
+			// keep updating lastScrollTop
+			lastScrollTop.current = containerRef.current.scrollTop
+
+			// 5 is for good measure since scroll* and clientHeight round dimensions
+			if ((containerRef.current.scrollTop + containerRef.current.clientHeight) + 5 >=
+				containerRef.current.scrollHeight) {
+				setDontImmediatelyScrollDown(false)
+			}
+		} 
+		else {
+			setShowScrollDown(false)
+		}
+	}
+
+	useEffect(() => {
+		if (!dontImmediatelyScrollDown) {
+			// if user scrolled to top, don't scroll to bottom
+			if (lastScrollTop.current > containerRef.current.scrollTop) {} 
+			else scroll()
+		}
 	})
 
 	return (
-		<div className="messages scrollbar-hidden" ref={msgRef}>
+		<div className="messages" ref={containerRef} onScroll={handleScroll}>
 			{messages.map((msg, i) => (
 				<Message key={i} message={msg} user={user} />
 			))}
+			{showScrollDown && (
+				<button className="strip-css-btn scroll-down-btn" onClick={scroll}>
+					<i className="fa fa-arrow-down"></i>
+				</button>
+			)}
 		</div>
 	)
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import ReactDom from "react-dom"
 import App from "./App"
 
 ReactDom.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	// <React.StrictMode>
+		<App />,
+	// </React.StrictMode>,
 	document.getElementById("root")
 )
